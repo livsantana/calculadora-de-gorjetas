57b776f499496d4ef60b541d3e33ca08a500595d
Lívia Santana <livviasantana@gmail.com>
2024-03-17 10:29:42 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let conta = 0
 let pessoas = 0
 
-const contaInput = document.querySelector(".conta")
+const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
+    calcular()
 }
 
 const pessoasInput = document.querySelector("#pessoas")
@@ -14,11 +15,12 @@ pessoasInput.addEventListener("input", receberQuantidadePessoas)
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector(".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
+
     if (evento.target.value === "0") {
         paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
-    } else{
-        paragrafoErro.style.display ="none"
+    } else {
+        paragrafoErro.style.display = "none"
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
@@ -26,18 +28,23 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento){
-    botoesGorjeta.forEach(botao=>{
-            botao.classList.remove('ativo')
-            
-        if(botao.value === evento.target.value){
-            botao.classList.add('ativo')
-        }
+function receberPorcentagem(evento){
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove('ativo')
     })
-
-    porcentagem = parseFloat(evento.target.value)/100
-
+    if (botao.value === evento.target.value) {
+            botao.classList.add('ativo')
+    }
 }
+
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)git a
